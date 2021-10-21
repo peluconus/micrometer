314eaf11e4d614b1fe1f217eba246195302bad1c
Peluconus <peluconus@hotmail.com>
2021-10-21 12:17:35 +0200

    Subida inicial del proyecto

---
+++ src/test/java/com/practica/micrometer/MicrometerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.micrometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrometerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
